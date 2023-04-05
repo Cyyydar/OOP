cf9f1654105db2e1baa88c046a702a42c3608e9e
Cyyydar <51922035+Cyyydar@users.noreply.github.com>
2023-04-05 11:41:23 +0300

    Lesson 7.7

---
+++ Lesson 7/7_5/main.cpp
@@ -0,0 +1,151 @@
+// Перегрузка оператора индексации
+#include <iostream>
+
+using namespace std;
+
+class DinMas{
+
+    int RazMas = 0;
+    int *Mas = nullptr;
+
+public:
+    DinMas(const DinMas &ob): Mas{nullptr}, RazMas{0} {
+        if((ob.Mas != nullptr) && (ob.RazMas != 0)){
+            Mas = new int [ob.RazMas];
+            RazMas = ob.RazMas;
+            for(int i = 0; i < ob.RazMas; i++)
+                Mas[i] = ob.Mas[i];
+        }
+        cout << "\n Constructor kopirovaniya \n";
+    }
+
+    DinMas& operator=(const DinMas &ob){
+        if(this == &ob)
+            return *this;
+        delete[]Mas;
+        Mas = nullptr;
+        RazMas = 0;
+        if((ob.Mas != nullptr) && (ob.RazMas != 0)) {
+            Mas = new int[ob.RazMas];
+            RazMas = ob.RazMas;
+            for(int i = 0; i < ob.RazMas; i++){
+                Mas[i] = ob.Mas[i];
+            }
+        }
+        cout << "\n Pereopredelennoe prisvoenie \n";
+        return *this;
+    }
+
+    DinMas(DinMas&&ob): Mas{nullptr}, RazMas{0} {
+        if((ob.Mas != nullptr) && (ob.RazMas != 0)){
+            Mas = ob.Mas;
+            RazMas = ob.RazMas;
+            ob.Mas = nullptr;
+            ob.RazMas = 0;
+        }
+        cout << "\n Kopirovanie peremesheniem \n";
+    }
+
+    DinMas&operator=(DinMas &&ob){
+        if(this == &ob)
+            return *this;
+        delete[]Mas;
+        Mas = nullptr;
+        RazMas = 0;
+        if((ob.Mas != nullptr) && (ob.RazMas != 0)) {
+            Mas = ob.Mas;
+            RazMas = ob.RazMas;
+            ob.Mas = nullptr;
+            ob.RazMas = 0;
+        }
+        cout << "\n Pereopredelennoe prisvoenie peremesheniem \n";
+        return *this;
+    }
+
+    DinMas(int k, int m): Mas{nullptr}, RazMas{0} {
+        cout << "\n Konstructor s parametrami \n";
+        if(k < 1)
+            return;
+        RazMas = k;
+        Mas = new int[RazMas];
+        for(int i = 0; i < RazMas; i++)
+            Mas[i] = m;
+    }
+
+    DinMas(): Mas{nullptr}, RazMas{0} {
+        cout << "\n Konstructor bez parametrov \n";
+    }
+
+    ~DinMas(){
+        delete[]Mas;
+        cout << "\n Destructor porabotal";
+    }
+
+    void Create (){
+        if(Mas != nullptr) {
+            cout << " Massiv uje zadan";
+            for(int i = 0; i < RazMas; i++)
+                cout << " " << Mas[i];
+            cout << "\n Chtobi perezapisat vvedi - 1\n";
+            cout << " Chtobi ne perezapisat vvedite - 0\n";
+            int f;
+            cin >> f;
+            if(f == 0)
+                return;
+        }
+        cout << "Vvedite razmer massiva (celoe chislo bolshe 0): ";
+        cin >> RazMas;
+        delete []Mas;
+        Mas = new int[RazMas];
+        cout << "Vvodite celie chisla: \n";
+        for(int i = 0; i < RazMas; i++) {
+            cout << " Mas [" << i << "] = ";
+            cin >> Mas[i];
+        }
+        cout << " Vvod zavershon \n";
+    }
+
+    void Show() const{
+        if(Mas == nullptr){
+            cout << " Massiv ne bil zadan \n";
+            return;
+        }
+        cout << " Vivod massiva: ";
+        for(int i = 0; i < RazMas; i++) {
+            cout << " " << Mas[i];
+        }
+        cout << "\n Adress nachala massiva: " << Mas << endl;
+    }
+
+    int& operator[](const int i){ // не безопасно т.к. можно выйти за пределы массива
+        return Mas[i];
+    } // следует осуществлять проверку на выход за пределы массива
+
+    const int& operator[](const int i) const {
+        return Mas[i];
+    }
+};
+
+int main()
+{
+    DinMas Massiv1(6, 6);
+    cout << "\n Massive1: \n";
+    Massiv1.Show();
+
+    const DinMas Massiv2(5,10);
+    cout << "\n Massive2: \n";
+    Massiv2.Show();
+
+    cout << "\n Massiv1[1] = 100 \n";
+    Massiv1[1] = 100;
+    Massiv1.Show();
+
+    //Massiv2[2] = -10; Ошибка: попытка изменить член cosnt объекта
+
+    cout << "\n Massiv2[2]=" << Massiv2[2] << endl;
+
+    cout << "\n Massiv2[10]=" << Massiv2[10] << endl;
+    return 0;
+}
+
+
